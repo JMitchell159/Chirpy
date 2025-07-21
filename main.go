fac94816d92e138fdb41953d10339ca4950c6c1b
JMichell159 <joemitchell159@gmail.com>
2025-07-21 06:34:01 -0500

    main now serves a website that says 'Welcome to Chirpy' on localhost:8080 as well as the logo on localhost:8080/assets/logo.png

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	var server http.Server
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	server.Handler = mux
 	server.Addr = ":8080"
 	server.ListenAndServe()
