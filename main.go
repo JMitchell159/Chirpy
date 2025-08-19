fbf1759d0147d1f13c854bc7007f9f0f96887b20
JMichell159 <joemitchell159@gmail.com>
2025-08-18 21:49:52 -0500

    split up front facing server, api server and admin server

--- main.go
+++ main.go
@@ -1,14 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handlerHealth(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(200)
+	message := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())
+	w.Write([]byte(message))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(200)
+}
+
+func (cfg *apiConfig) createServer() *http.Server {
 	mux := http.NewServeMux()
-	var server http.Server
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	server.Handler = mux
-	server.Addr = ":8080"
+	mux.HandleFunc("GET /api/healthz", handlerHealth)
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", cfg.handlerReset)
+	return &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+}
+
+func main() {
+	apiCfg := &apiConfig{}
+	apiCfg.fileserverHits.Store(0)
+	server := apiCfg.createServer()
 	server.ListenAndServe()
 }
